06506486bfa5b666666dc151bce954470fde77cf
shinsujin <pofoxz@gmail.com>
2026-05-09 19:57:58 +0900

    add: 문자열형

---
+++ modern-javascript/string.js
@@ -0,0 +1,13 @@
+const lang = "javascript";
+const love = "사랑해";
+const you = null;
+const num = 5;
+console.log(lang + null + love + num);
+
+// 문자열 길이 구하기
+const sticky = "howudointextmenow궁금하지everyslice";
+console.log(sticky.length);
+
+// 따옴표 혼용
+const mango = "how u doin text me now '궁금하지' every slice";
+console.log(mango);
