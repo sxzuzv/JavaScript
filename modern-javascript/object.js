9ddd8597e917213c470d2cea52c16bc6ce056d7e
shinsujin <pofoxz@gmail.com>
2026-05-09 23:20:05 +0900

    add: 오브젝트형

---
+++ modern-javascript/object.js
@@ -0,0 +1,23 @@
+const msg = {
+    name: '망고 스티키 라이스',
+    comment: '찰밥처럼 찰싹 달라 붙어요!',
+    age: 1
+};
+
+const array = [];
+array.push(msg);
+
+console.log(array);
+
+// (정석) 오브젝트형 데이터 생성 시에는 new 연산자 사용
+let obj = new Object();
+obj = {
+    name: '망고 스티키 라이스',
+    comment: '찰밥처럼 찰싹 달라 붙어요!',
+    age: 1
+};
+
+let arr = new Array();
+arr.push(msg);
+
+console.log(arr);
