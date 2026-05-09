09bbee606acf1cf15b2bea01d79bb14aa47dd6e7
shinsujin <pofoxz@gmail.com>
2026-05-10 00:00:11 +0900

    add: 빼기, 곱하기, 나누기 연산자

--- modern-javascript/operator/arithmetic.js
+++ modern-javascript/operator/arithmetic.js
@@ -16,3 +16,12 @@ console.log(3 + msg); // 숫자가 문자열의 속성으로 변경
 console.log(+'628' + 1); // 629
 console.log(+true);      // 1
 console.log(+false);     // 0
+
+// 빼기, 곱하기, 나누기 연산자
+// 숫자 형태로 작성된 문자열은 숫자형으로 자동 변환하여 연산
+console.log('10' - 3);
+console.log('10' * 3);
+console.log('10' / 3);
+console.log('10' - '3');
+console.log('10' * '3');
+console.log('10' / '3');
