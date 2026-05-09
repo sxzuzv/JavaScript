655ec70a489bc12d5672f149d2a70895ac98aeb1
shinsujin <pofoxz@gmail.com>
2026-05-09 19:43:14 +0900

    add: 숫자형

---
+++ modern-javascript/number.js
@@ -0,0 +1,17 @@
+const myAge = 22;
+const myHeight = 174.5;
+console.log(typeof myAge);
+console.log(typeof myHeight);
+
+// 숫자형의 기본적인 산술 연산
+const kuk = 95;
+const eng = 85;
+const math = 90;
+const total = kuk + eng + math;
+const avg = total / 3;
+console.log(total);
+console.log(avg);
+
+// NaN
+const num = '진'/10;
+console.log(num);
