3e5f03a5592e9f403260d7198a736f06dd9c6b44
shinsujin <pofoxz@gmail.com>
2026-05-09 20:00:44 +0900

    add: 불리언형

---
+++ modern-javascript/boolean.js
@@ -0,0 +1,10 @@
+const checkIn = true;
+const checkOut = false;
+console.log(checkIn);
+console.log(checkOut);
+
+// 비교문에서 주로 사용
+let bigger = 100 > 50;
+console.log(bigger);
+bigger = 100 < 50;
+console.log(bigger);
