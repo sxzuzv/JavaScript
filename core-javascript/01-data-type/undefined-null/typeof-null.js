d478171e2ca9e1cb864ca720c2b30c1b03c920ea
shinsujin <pofoxz@gmail.com>
2026-06-25 22:51:16 +0900

    add: [undefined와 null] typeof null

---
+++ core-javascript/01-data-type/undefined-null/typeof-null.js
@@ -0,0 +1,9 @@
+// typeof null은 object
+
+let n = null;
+console.log(typeof n);  // object
+
+console.log(n == undefined);    // true
+console.log(n == null);         // true
+console.log(n === undefined);   // false
+console.log(n === null);        // true
