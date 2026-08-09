755c7e8c8f0e5ef2afb505ff9670236b43c95cb6
shinsujin <pofoxz@gmail.com>
2026-08-09 20:35:44 +0900

    add: [클래스 상속] 클래스 상속 및 추상화 방법 (1)
    - 인스턴스 생성 후 프로퍼티 제거

---
+++ core-javascript/07-class/inheritance/abstraction/inheritance-and-abstraction-1.js
@@ -0,0 +1,35 @@
+// 클래스 상속 및 추상화 방법 (1) - 인스턴스 생성 후 프로퍼티 제거
+
+var extendClass1 = function (SuperClass, SubClass, subMethods) {
+    SubClass.prototype = new SuperClass();
+    for (var prop in SubClass.prototype) {
+        if (SubClass.prototype.hasOwnProperty(prop)) {
+            delete SubClass.prototype[prop];
+        }
+    }
+
+    if (subMethods) {
+        for (var method in subMethods) {
+            SubClass.prototype[method] = subMethods[method];
+        }
+    }
+    
+    Object.freeze(SubClass.prototype);
+    return SubClass;
+};
+
+var Rectangle = function (width, height) {
+    this.width = width;
+    this.height = height;
+};
+Rectangle.prototype.getArea = function () {
+    return this.width * this.height;
+};
+var rect = new Rectangle(3, 4);
+console.log(rect.getArea());    // 12
+
+var Square = extendClass1(Rectangle, function (width) {
+    Rectangle.call(this, width, width);
+});
+var sq = new Square(5);
+console.log(sq.getArea());      // 25
