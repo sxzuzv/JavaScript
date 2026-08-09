4a0c2320cce71f019c08a59301a52aaabc23d549
shinsujin <pofoxz@gmail.com>
2026-08-10 01:38:30 +0900

    add: [클래스 상속] 클래스 상속 및 추상화 방법 (3)
    - Object.create 활용

---
+++ core-javascript/07-class/inheritance/abstraction/inheritance-and-abstraction-3.js
@@ -0,0 +1,28 @@
+// 클래스 상속 및 추상화 방법 (3) - Object.create 활용
+
+var extendClass3 = function (SuperClass, SubClass, subMethods) {
+    SubClass.prototype = Object.create(SuperClass.prototype);
+
+    if (subMethods) {
+        for (var method in subMethods) {
+            SubClass.prototype[method] = subMethods[method];
+        }
+    }
+    
+    Object.freeze(SubClass.prototype);
+    return SubClass;
+};
+
+var Rectangle = function (width, height) {
+    this.width = width;
+    this.height = height;
+};
+Rectangle.prototype.getArea = function () {
+    return this.width * this.height;
+};
+
+var Square = extendClass3(Rectangle, function (width) {
+    Rectangle.call(this, width, width);
+});
+var sq = new Square(5);
+console.log(sq.getArea());      // 25
